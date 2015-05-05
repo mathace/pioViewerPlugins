ef96edd93df388dc7e031a956fb3057993397ee2
Kuba <kuba.straszewski@gmail.com>
2015-05-05 22:49:20 +0200

    Initial commit with few plugins

---
+++ PioViewerPlugins/AggregatedAnalysisPlugin.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using PioViewerApi.Plugin;
+using PioViewerApi.Server;
+using PioViewerApi.Util;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimplePlugin
+{
+    public class AggregatedAnalysis : IServerPlugin
+    {
+        #region Identification
+        public string Name
+        {
+            get { return "Runouts aggregated frequencies analysis"; }
+        }
+
+        public string Category
+        {
+            get { return "Aggregation"; }
+        }
+        #endregion
+
+        #region plugin initialization
+        protected IServerPluginContext Context { get; private set; }
+
+        protected IServerPlayer OOP, IP;
+
+        public void Initialize(IServerPluginContext context)
+        {
+            this.Context = context;
+            this.OOP = context.ServerUtils.OOP;
+            this.IP = context.ServerUtils.IP;
+        }
+        #endregion
+
+        public void Execute()
+        {
+            var node = Context.Controller.SelectedNode;
+            if (node == null)
+            {
+                throw new InvalidOperationException("The aggregated analysis can only be run when action node is selected");
+            }
+            var report = new StringBuilder();
+
+            var root = Context.ServerWrapper.ShowRootNode();
+            var rootMatchups = Context.ServerWrapper.CalcEquityInNode(IP, root).TotalMatchups();
+
+            string line = GenerateLineString(node);
+            var fileName = Context.Controller.CurrentFileName;
+            GenerateReportIntro(node, report, line, fileName);
+            GenerateReport(node, report, rootMatchups);
+            WriteReportToFile(node, report, line);
+        }
+
+        /// <summary>
+        /// Looks for all the nodes with the same action line, but different board cards.
+        /// </summary>
+        private List<IServerNode> FindSimilarNodesOnOtherBoards(IServerNode node)
+        {
+            Stack<IServerNode> fullPath = new Stack<IServerNode>();
+            for (IServerNode temp = node; temp.Parent != null; temp = temp.Parent)
+            {
+                fullPath.Push(temp);
+            }
+
+            List<IServerNode> allSimilar = new List<IServerNode>();
+            allSimilar.Add(fullPath.Pop());
+            List<IServerNode> tempList = new List<IServerNode>();
+            while (fullPath.Count > 0)
+            {
+                var pattern = fullPath.Pop();
+                foreach (var item in allSimilar)
+                {
+                    var itemChildren = Context.ServerWrapper.ShowChildren(item);
+                    foreach (var child in itemChildren)
+                    {
+                        if (child.NodeText == pattern.NodeText || item.NodeType == "SPLIT_NODE")
+                        {
+                            tempList.Add(child);
+                        }
+                    }
+                }
+                allSimilar = tempList;
+                tempList = new List<IServerNode>();
+            }
+            return allSimilar;
+        }
+
+        private class ReportLine
+        {
+            public Card[] Board { get; set; }
+            public double EVOOPSums { get; set; }
+            public double EVOOPMatchups { get; set; }
+            public double EVIPSums { get; set; }
+            public double EVIPMatchups { get; set; }
+
+            public double EQOOPSums { get; set; }
+            public double EQOOPMatchups { get; set; }
+            public double EQIPSums { get; set; }
+            public double EQIPMatchups { get; set; }
+
+            public List<string> ActionNames { get; set; }
+            public Dictionary<string, double> ActionFrequencies { get; set; }
+
+            public ReportLine()
+            {
+                ActionFrequencies = new Dictionary<string, double>();
+                ActionNames = new List<string>();
+            }
+        }
+
+        /// <summary>
+        /// Get the results from one particular node into the structure
+        /// </summary>
+        private ReportLine CreateReportLineForNode(IServerNode node)
+        {
+            var result = new ReportLine();
+
+            //the board
+            result.Board = node.Board;
+
+            // Calculate IP and OOP ev and equity in the node.
+            // The result for EV calculation is two ranges
+            // one range is sums and the other is matchups
+            // EV for a given hand can be calculated by dividing
+            // sums by matchups.
+            // The total EV in node can be obtained by diving
+            // total sums in the node by total counters
+            var evIP = Context.ServerWrapper.CalcEV(IP, node);
+            var evOOP = Context.ServerWrapper.CalcEV(OOP, node);
+
+            result.EVOOPSums = evOOP.TotalWins();
+            result.EVOOPMatchups = evOOP.TotalMatchups();
+            result.EVIPSums = evIP.TotalWins();
+            result.EVIPMatchups = evIP.TotalMatchups();
+        
+            var eqIP = Context.ServerWrapper.CalcEquityInNode(IP, node);
+            var eqOOP = Context.ServerWrapper.CalcEquityInNode(OOP, node);
+
+            result.EQOOPSums = eqOOP.TotalWins();
+            result.EQOOPMatchups = eqOOP.TotalMatchups();
+            result.EQIPSums = eqIP.TotalWins();
+            result.EQIPMatchups = eqIP.TotalMatchups();
+
+            // If the node is action node (e.g. - not a street split node nor final node)
+            // then we also compute how often given action is selected
+            if (node.ActionPlayer != null)
+            {
+                // we ask server to list all children of a current node
+                var actions = Context.ServerWrapper.ShowChildren(node);
+                foreach (var action in actions)
+                {
+                    // for each child we ask for the range of an action player
+                    var rangeForAction = Context.ServerWrapper.ShowRange(node.ActionPlayer, action);
+                    // and store it in the result dictionary
+                    // NodeText property denotes the action (e.g. CHECK or BET 100)
+                    result.ActionFrequencies[action.NodeText] = rangeForAction.TotalWeights();
+                    // Node name is stored separately to preserve the action's order
+                    result.ActionNames.Add(action.NodeText);
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Sum the results in a single structure
+        /// </summary>
+        /// <param name="fullReport"></param>
+        /// <returns></returns>
+        private ReportLine GenerateAggregatedReport(IServerNode node, List<ReportLine> fullReport)
+        {
+            var result = new ReportLine()
+            {
+                Board = new Card[node.Board.Length]
+            };
+
+            if (fullReport.Count > 0)
+            {
+                result.ActionNames = fullReport[0].ActionNames;
+            }
+
+            foreach (var line in fullReport)
+            {
+                foreach (var actionName in line.ActionFrequencies.Keys)
+                {
+                    if (!result.ActionFrequencies.ContainsKey(actionName))
+                    {
+                        result.ActionFrequencies[actionName] = 0;
+                    }
+                    result.ActionFrequencies[actionName] += line.ActionFrequencies[actionName];
+                }
+                result.EVIPMatchups += line.EVIPMatchups;
+                result.EVIPSums += line.EVIPSums;
+                result.EVOOPMatchups += line.EVOOPMatchups;
+                result.EVOOPSums += line.EVOOPSums;
+
+                result.EQIPMatchups += line.EQIPMatchups;
+                result.EQIPSums += line.EQIPSums;
+                result.EQOOPMatchups += line.EQOOPMatchups;
+                result.EQOOPSums += line.EQOOPSums;
+            }
+            return result;
+        }
+
+
+        private List<String> GenerateHeaderNames(List<string> actionNames, int boardLen)
+        {
+            var result = new List<String>();
+            var streets = new String[] { "Flop", "Turn", "River" };
+            for (int i = 3; i <= boardLen; i++)
+            {
+                result.Add(streets[i - 3]);
+            }
+
+            result.Add("Global %");
+            result.Add("OOP Equity");
+            result.Add("IP Equity");
+            result.Add("OOP EV");
+            result.Add("IP EV");
+            foreach (var actionName in actionNames)
+            {
+                result.Add(actionName);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Generates a list of calvulated values for the columns in a single line of a report
+        /// </summary>
+        private List<String> GenerateReportLine(ReportLine reportLine, double rootMatchups)
+        {
+            var result = new List<String>();
+
+            AddBoard(result, reportLine.Board);
+            result.Add((reportLine.EQOOPMatchups * 100 / rootMatchups).ToString("G6"));
+            result.Add((reportLine.EQOOPSums / reportLine.EQOOPMatchups).ToString("G6"));
+            result.Add((reportLine.EQIPSums / reportLine.EQIPMatchups).ToString("G6"));
+            result.Add((reportLine.EVOOPSums / reportLine.EVOOPMatchups).ToString("G6"));
+            result.Add((reportLine.EVIPSums / reportLine.EVIPMatchups).ToString("G6"));
+            double[] frequencies = new double[reportLine.ActionNames.Count];
+            for (int i = 0; i < frequencies.Length; i++)
+            {
+                var actionName = reportLine.ActionNames[i];
+                frequencies[i] = reportLine.ActionFrequencies[actionName];
+            }
+            double sum = frequencies.Sum();
+            foreach (var f in frequencies)
+            {
+                result.Add((f * 100 / sum).ToString("G3"));
+            }
+            return result;
+        }
+
+        private void AddBoard(List<String> result, Card[] board)
+        {
+            if (board.Length >= 3)
+            {
+                result.Add(board[0] + " " + board[1] + " " + board[2]);
+            }
+            if (board.Length >= 4)
+            {
+                result.Add("" + board[3]);
+            }
+            if (board.Length >= 5)
+            {
+                result.Add("" + board[4]);
+            }
+        }
+
+        /// <summary>
+        /// Generate the full report
+        /// </summary>
+        private void GenerateReport(IServerNode nodeForReport, StringBuilder report, double rootMatchups)
+        {
+            report.AppendLine("");
+
+            var similarNodes = FindSimilarNodesOnOtherBoards(nodeForReport);
+            var results = new List<ReportLine>();
+            foreach (var singleNode in similarNodes)
+            {
+                results.Add(CreateReportLineForNode(singleNode));
+            }
+            var aggregatedResult = GenerateAggregatedReport(nodeForReport, results);
+            report.AppendLine(string.Join(",", GenerateHeaderNames(aggregatedResult.ActionNames, nodeForReport.Board.Length)));
+            report.AppendLine(string.Join(",", GenerateReportLine(aggregatedResult, rootMatchups)));
+            foreach (var resultLine in results)
+            {
+                report.AppendLine(string.Join(",", GenerateReportLine(resultLine, rootMatchups)));
+            }
+        }
+
+        private static void WriteReportToFile(IServerNode node, StringBuilder report, string line)
+        {
+            if (!Directory.Exists("Reports"))
+            {
+                Directory.CreateDirectory("Reports");
+            }
+            var path = Path.Combine("Reports", "AggregatedReport_" + DateTime.Now.ToString("yyyy-MM-dd_hh-mm-ss") + ".csv");
+            File.WriteAllText(path, report.ToString());
+        }
+
+        private static void GenerateReportIntro(IServerNode node, StringBuilder report, string line, string path)
+        {
+            report.AppendLine("Aggregation analysis");
+            report.AppendLine("File, " + path);
+            report.AppendLine("Action Line," + line);
+        }
+
+        private static string GenerateLineString(IServerNode node)
+        {
+            string line = node.NodeText;
+            var parent = node.Parent;
+            while (parent.Parent != null)
+            {
+                line = parent.NodeText + " " + line;
+                parent = parent.Parent;
+            }
+            return line;
+        }
+    }
+}
---
+++ PioViewerPlugins/SelectRandomChildNodePlugin.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PioViewerApi.Plugin;
+using PioViewerApi.Server;
+
+namespace SimplePlugin
+{
+    /// <summary>
+    /// This is an example plugin that shows how to browse a tree.
+    /// When plugin is started it creates a new Form with a button and returns the control to the Browser.
+    /// The form is then visible and can interoperate with the browser, while user can keep normally using
+    /// the viewer.
+    /// 
+    /// When the button is clicked the plugin selects randomly one of the children of the node it previously selected.
+    /// </summary>
+    public class SelectRandomChildNodePlugin : IServerPlugin
+    {
+
+        public string Name
+        {
+            get { return "Select random children!"; }
+        }
+
+        public string Category
+        {
+            get { return "Tutorial"; }
+        }
+
+        protected IServerPluginContext Context { get; private set; }
+
+        public void Initialize(IServerPluginContext context)
+        {
+            this.Context = context;
+        }
+
+        public void Execute()
+        {
+            new TreeBrowsingForm(Context).Show();
+        }
+
+
+        class TreeBrowsingForm : Form
+        {
+            IServerNode CurrentNode { get; set; }
+
+            IServerPluginContext Context { get; set; }
+
+            private void InitializeComponent()
+            {
+                Button b = new Button();
+                b.Text = "Select random child";
+                b.Location = new System.Drawing.Point(10, 10);
+                b.AutoSize = true;
+                b.Click += b_Click;
+
+                this.Size = new System.Drawing.Size(500, 500);
+                this.Controls.Add(b);
+            }
+
+            public TreeBrowsingForm(IServerPluginContext context)
+            {
+                InitializeComponent();
+                this.Context = context;
+                this.CurrentNode = context.ServerWrapper.ShowRootNode();
+                this.Text = CurrentNode.NodeId;
+            }
+
+            void b_Click(object sender, EventArgs e)
+            {
+                var children = Context.ServerWrapper.ShowChildren(CurrentNode).ToList();
+
+                if (children.Count == 0)
+                {
+                    MessageBox.Show("Node " + CurrentNode.NodeId + " has no children");
+                }
+                else
+                {
+                    var randomChild = children[new Random().Next(children.Count)];
+                    this.CurrentNode = randomChild;
+                    this.Context.Controller.SelectedNode = randomChild;
+                    this.Text = randomChild.NodeId;
+                }
+            }
+        }
+    }
+
+}
